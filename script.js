c2fe824ae381877b9fd38d0cd4022bdbf7d1a4a1
khadija_mt <mmoutahmilkhadija@gmail.com>
2026-02-16 10:21:19 +0000

    Add ROI calculation function in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Exemple de fonction pour calculer le gain potentiel
+function calculerROI(prixProjetMoyen) {
+    const augmentationConversion = 0.20; // +20% de leads grâce au design Figma 2026
+    return prixProjetMoyen * augmentationConversion;
+}
+
+console.log("BoostMark Plateforme - Prête pour les freelances");
