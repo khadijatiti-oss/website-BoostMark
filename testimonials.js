9655ec1c293d09601144a596b1ad99fe0146b9e3
khadija_mt <mmoutahmilkhadija@gmail.com>
2026-02-20 06:27:53 +0000

    Delete testimonials.js

--- testimonials.js
+++
@@ -1,6 +0,0 @@
-window.testimonialsData = [
-  { quote: "Site live en 7j, 1er client gagné semaine 2", author: "Sarah - Graphiste, Casablanca", avatar: "avatar1.jpg" },
-  { quote: "Portfolio pro + leads auto, ROI immédiat", author: "Youssef - Dev Fullstack, Rabat", avatar: "avatar2.jpg" },
-  { quote: "Support 24/7, qualité agence prix freelance", author: "Fatima - Marketeuse, Marrakech", avatar: "avatar3.jpg" },
-  { quote: "De freelance solo à agence en 1 mois", author: "Hamza - Consultant, Fès", avatar: "avatar4.jpg" }
-];
